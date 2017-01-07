299fd5d7f27be671b2d6443f35deffc8bd146c99
Marek Hulan <mhulan@redhat.com>
2017-01-07 11:04:46 +0100

    Update dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email 'ares@igloonet.com'
 license          'All rights reserved'
 description      'Installs/Configures dev_virtual_machine'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.6'
+version          '0.2.7'
 
 depends	'user', '>= 0.1.0'
+depends 'nfs'
